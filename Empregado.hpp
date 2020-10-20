dbda2448f409d0a937d182cd5eb1b545ddbb16c4
biafpaiva <61837675+biafpaiva@users.noreply.github.com>
2020-10-20 16:49:01 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -9,15 +9,14 @@ class Empregado {
   public:
   
     double pagamentoMes(double horasTrabalhadas);
-	  Empregado(double _salarioHora, std::string _nome) : salarioHora(_salarioHora), nome(_nome) {};
+    Empregado(double _salarioHora, std::string _nome) : salarioHora(_salarioHora), nome(_nome) {};
     void imprimeEmp(double horasTrabalhadas);
 
   protected:
 
     double salarioHora; 
     std::string nome;
-    double horasTrabalhadas;
-
+	
 };
 
 #endif
